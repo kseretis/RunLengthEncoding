7cba8502727c5f38edb7872689908c028c8e74d4
kseretis <serkleanthis@gmail.com>
2025-04-04 11:19:38 +0300

    RLE class created

---
+++ RLE.cs
@@ -0,0 +1,18 @@
+﻿namespace RunLengthEncoding
+{
+    public static class RLE
+    {
+        public const char Splitter = '|';
+        private const string RegexPattern = @"\d+\D";
+
+        public static string Encode(string text)
+        {
+            return string.Empty;
+        }
+
+        public static string Decode(string text)
+        {
+            return string.Empty;
+        }
+    }
+}
