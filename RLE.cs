c0da17699fda0a0c86e993877e1a98443ab1e5ed
kseretis <serkleanthis@gmail.com>
2025-04-04 11:32:41 +0300

    Updates

--- RLE.cs
+++ RLE.cs
@@ -3,16 +3,70 @@
     public static class RLE
     {
         public const char Splitter = '|';
-        private const string RegexPattern = @"\d+\D";
+        internal const string RegexPattern = @"\d+\D";
 
         public static string Encode(string text)
         {
-            return string.Empty;
+            var encodedText = String.Empty;
+            var previousChar = text[0];
+            var count = 1;
+
+            for (var c = 1; c < text.Length; c++)
+            {
+                var currentChar = text[c];
+
+                if (currentChar.Equals(previousChar))
+                {
+                    count++;
+                }
+                else
+                {
+                    encodedText = UpdateEncodedValue(encodedText, count, previousChar);
+                    previousChar = currentChar;
+                    count = 1;
+                }
+
+                if (c == text.Length - 1)
+                {
+                    encodedText = UpdateEncodedValue(encodedText, count, previousChar);
+                }
+            }
+
+            return encodedText;
         }
 
         public static string Decode(string text)
         {
-            return string.Empty;
+            string decodedText = string.Empty;
+
+            string[] parts = text.SplitText();
+
+            foreach (var part in parts)
+            {
+                decodedText = UpdateDecodedValue(decodedText, part);
+            }
+
+            return decodedText;
         }
+
+        #region Helpers
+
+        private static string UpdateEncodedValue(string encodedValue, int count, char character)
+        {
+            string text = Char.IsDigit(character)
+                ? string.Concat(character, Splitter)
+                : character.ToString();
+
+            return string.Concat(encodedValue, count, text);
+        }
+
+        private static string UpdateDecodedValue(string decodedValue, string textPart)
+        {
+            (char character, int occurences) = textPart.SplitPart();
+
+            return string.Concat(decodedValue, new String(character, occurences));
+        }
+
+        #endregion
     }
 }
